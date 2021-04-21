ed40d9d99f4ff49a3c1dd540906cddff0d7aba5e
Michael-Blackwell <77996176+Michael-Blackwell@users.noreply.github.com>
2021-04-21 14:04:55 -0400

    Update main.py
    updated to extract db features and save them to disk

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 from variables import *
 
-
-# import images as datasets
-test_path = main_dir + '/imagenet-mini/temp'
-
 # import labels for train & val datasets
-labels_df = import_data(main_dir)
+labels_df = import_class_labels(main_dir)
 class_names = labels_df.Label.tolist()
 
 # initiate model
-model, feat_extractor = load_model(numclasses)
+model, feat_extractor = load_model(len(class_names))
 
-# import image dataset from disk, preserving image path names
-image_names, main_ds = import_images(val_data_path)
-
-# extract the images features
-imgs_features = feat_extractor.predict(main_ds)  # TODO make sure order of images is retained
-del main_ds  # delete after features are extracted to save space.
+# import dataset from disk and extract image features.
+features_df = import_images(train_data_path, feat_extractor)
 print("features extracted!")
 
-features_df = pd.DataFrame(imgs_features, index=image_names)
-
-predict(test_img_dir, feat_extractor, features_df)
+# Pickle features df to disk so extraction is not necessary each time.
+features_df.to_pickle(pickling_path)
